750e7074ddd9a619985cbe2451ef0c88beac6764
Apostolique <vynecompiler@gmail.com>
2015-08-23 18:28:12 -0400

    First commit!

---
+++ AposBotSettings.py
@@ -0,0 +1,13 @@
+KEY = "" #I believe that was the Riot API key
+HOST="irc.twitch.tv"
+PORT=6667
+NICK="AposBot"
+IDENT="AposBot"
+REALNAME="AposBot"
+CHANNEL="apostolique"
+PASSWORD="lalalalalalala" #From http://twitchapps.com/tmi/ without oauth:
+CURRENTACCOUNT = "apostolique"
+CHATDATABASE = "chatData.json"
+COMMANDDATABASE = "customCommands.json"
+
+bot_mod = ["apostolique"]
