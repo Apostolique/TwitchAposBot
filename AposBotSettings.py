8f7e6f51b27d139b5e152b18ec29945aba6919e9
Apostolique <vynecompiler@gmail.com>
2015-08-23 18:30:18 -0400

    License

--- AposBotSettings.py
+++ AposBotSettings.py
@@ -1,3 +1,15 @@
+"""This file is part of TwitchAposBot.
+TwitchAposBot is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+TwitchAposBot is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with TwitchAposBot.  If not, see <http://www.gnu.org/licenses/>."""
+
 KEY = "" #I believe that was the Riot API key
 HOST="irc.twitch.tv"
 PORT=6667
